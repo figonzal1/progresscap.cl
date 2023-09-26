dd35691ebaa70ed290837b4ff258f448e0d39d99
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-09-25 22:40:59 -0300

    script revisa si link se abre desde movil

--- validarcerts.php
+++ validarcerts.php
@@ -1,4 +1,22 @@
 <?php
+
+function es_movil()
+{
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+    $moviles = array(
+        'Android', 'iPhone', 'Windows Phone', 'iPod', 'BlackBerry', 'iPad', 'Symbian'
+    );
+
+    foreach ($moviles as $movil) {
+        if (strpos($user_agent, $movil) !== false) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 // Comprueba si el directorio y el nombre del archivo están establecidos
 if (isset($_GET['curso']) && isset($_GET['diploma'])) {
 
@@ -12,12 +30,22 @@ if (isset($_GET['curso']) && isset($_GET['diploma'])) {
     // Verifica si el curso y el archivo son válidos
     if (preg_match('/^[a-z0-9-_]+$/i', $curso) && preg_match('/^[a-z0-9-_]+$/i', $archivo)) {
 
+
+        if (es_movil()) {
+            $prefix = 'https://docs.google.com/gview?embedded=true&url=https://www.progresscap.cl/';
+        } else {
+            $prefix = "https://www.progresscap.cl/";
+        }
+
         // Construye la ruta al archivo
-        $ruta = 'certificados/' . $curso . '/' . $archivo . ".pdf";
+        $postRutaFull = 'certificados/' . $curso . '/' . $archivo . ".pdf";
 
         // Comprueba si el archivo existe
-        if (file_exists($ruta)) {
+        if (file_exists($postRutaFull)) {
+
             // Redirige al archivo
+            $postRuta = 'certificados/' . $curso . '/' . $archivo;
+            $ruta = $prefix . $postRuta;
             header('Location: ' . $ruta);
         } else {
             echo 'El diploma que intentas acceder, no existe.';
@@ -28,3 +56,10 @@ if (isset($_GET['curso']) && isset($_GET['diploma'])) {
 } else {
     echo 'Por favor, proporciona el curso y el nombre del diploma.';
 }
+
+/*
+if (es_movil()) {
+    echo "El cliente es un dispositivo móvil.";
+} else {
+    echo "El cliente no es un dispositivo móvil.";
+}*/
